191c44ee8d85291d27c93b563607e08b2a51110d
Dat <s3819827@rmit.edu.vn>
2022-09-08 14:45:18 +0700

    Initial Commit

---
+++ Musicapp/MusicappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicappApp.swift
+//  Musicapp
+//
+//  Created by dat on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
